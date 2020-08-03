43cd355b6249172cd1b07d8b9fdbb2b6c0799480
Stefan <stefan.szabo@travelperk.com>
2020-08-03 09:40:24 +0200

    fixed flake8 warnings

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -31,7 +31,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         ingredients = validated_data.pop('ingredients')
 
-        super(RecipeSerializer,self).update(instance,validated_data)
+        super(RecipeSerializer, self).update(instance, validated_data)
 
         Ingredient.objects.filter(recipe=instance).delete()
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets,mixins
+from rest_framework import viewsets
 
 from core.models import Recipe
 
